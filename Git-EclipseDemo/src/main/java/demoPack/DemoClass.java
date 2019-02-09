0f678cf61a78bdccb52e5481cd354a47b014cdfc
JigarSony <sonijigar94@gmail.com>
2019-02-09 12:32:28 +0530

    Commit-1 -Push

---
+++ Git-EclipseDemo/src/main/java/demoPack/DemoClass.java
@@ -0,0 +1,12 @@
+package demoPack;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		/*
+		 * 
+		 * Hello This is Just Demo for Git and Eclipse Integration
+		 */
+	}
+
+}
